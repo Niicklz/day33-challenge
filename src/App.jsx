e951f560e5297d09a057238a361e0d803703e2f3
Jose Vasquez <josevasquez8365@gmail.com>
2023-09-27 23:09:07 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import "./styles.css"
-import { Note } from './components/Note/Note'
+import React, { useState } from 'react';
+import "./styles.css";
+import { Note } from './components/Note/Note';
+
 export const App = () => {
+  const [notes, setNotes] = useState(() => {
+    const notesFromStorage = window.localStorage.getItem('notes')
+    if (notesFromStorage) return JSON.parse(notesFromStorage)
+    return []
+  })
+
+  const addNote = () => {
+    setNotes((prevNotes) => [...prevNotes, { id: Date.now(), text: "" }])
+  }
+
+  const editNote = (newText, id) => {
+    const updatedNotes = [...notes];
+    const noteIndex = updatedNotes.findIndex((note) => note.id === id);
+    if (noteIndex !== -1) {
+      updatedNotes[noteIndex] = { id, text: newText };
+      localStorage.setItem("notes", JSON.stringify(updatedNotes));
+      setNotes(updatedNotes);
+    }
+  };
+
+  const deleteNote = (id) => {
+    const updatedNotes = notes.filter((note) => note.id !== id);
+    localStorage.setItem("notes", JSON.stringify(updatedNotes));
+    setNotes(updatedNotes);
+  };
+
   return (
     <div className='container'>
-     <Note/>
+      <button className='add' onClick={addNote}>+ Agregar nota</button>
+      {notes.length > 0 && notes.map((note) => (
+        <Note key={note.id} val={note.text} id={note.id} onEditNote={editNote} onDeleteNote={deleteNote} />
+      ))}
     </div>
   )
 }
--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -1,16 +1,38 @@
-import React from 'react'
-import "./Note.css"
-import {BsFillTrashFill} from "react-icons/bs";
-import {PiNotePencilBold} from "react-icons/pi";
+import { useEffect, useState } from "react";
+import "./Note.css";
+import { BsFillTrashFill } from "react-icons/bs";
+import { PiNotePencilBold } from "react-icons/pi";
 
-export const Note = () => {
+export const Note = ({ val, id, onEditNote, onDeleteNote }) => {
+  const [textareaValue, setTextareaValue] = useState(val || "");
+
+  const editNote = () => {
+    onEditNote(textareaValue, id);
+  };
+
+  const deleteNote = () => {
+    onDeleteNote(id);
+  };
+
   return (
-    <article className='note-container'>
-        <header>
-            <button className="edit"><PiNotePencilBold/></button>
-            <button className="delete"><BsFillTrashFill/></button>
-        </header>
-        <textarea name="" spellcheck="false" id="" cols="30" rows="10"></textarea>
+    <article className="note-container">
+      <header>
+        <button className="edit">
+          <PiNotePencilBold className="icon" onClick={editNote} />
+        </button>
+        <button className="delete">
+          <BsFillTrashFill className="icon" onClick={deleteNote} />
+        </button>
+      </header>
+      <textarea
+        value={textareaValue}
+        onChange={(e) => setTextareaValue(e.target.value)}
+        name=""
+        spellCheck="false"
+        id=""
+        cols="30"
+        rows="10"
+      ></textarea>
     </article>
-  )
-}
+  );
+};
